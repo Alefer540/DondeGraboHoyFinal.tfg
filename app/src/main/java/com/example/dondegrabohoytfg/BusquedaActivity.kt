399e546b2d6b423a660af0b2dd139b0b460fc038
Alejandro <alefer540@alu.centroafuera.es>
2022-06-03 14:04:55 +0200

    tutoria 03/06/2022

--- app/src/main/java/com/example/dondegrabohoytfg/BusquedaActivity.kt
+++ app/src/main/java/com/example/dondegrabohoytfg/BusquedaActivity.kt
@@ -1,17 +1,43 @@
 package com.example.dondegrabohoytfg
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dondegrabohoytfg.databinding.ActivityBusquedaBinding
-import com.example.dondegrabohoytfg.databinding.ActivityMainBinding
+
 
 
 class BusquedaActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityBusquedaBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBusquedaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bundle = intent.extras
+        val Ciudad = bundle?.getString("Ciudad")
+        val Espacio = bundle?.getString("Espacio")
+        val AbiertoCerrado = bundle?.getString("AbiertoCerrado")
+
+
+
+        binding.tvAoC.text=AbiertoCerrado
+        binding.tvCiudad.text=Ciudad
+        binding.tvTipo.text=Espacio
+
+
+
+
+        binding.botonVolver.setOnClickListener {
+            val volverIntent = Intent(this, MainActivity::class.java)
+
+            startActivity(volverIntent)
+        }
+
     }
 }
--- app/src/main/java/com/example/dondegrabohoytfg/MainActivity.kt
+++ app/src/main/java/com/example/dondegrabohoytfg/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.dondegrabohoytfg
 
-import android.content.Context
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dondegrabohoytfg.databinding.ActivityMainBinding
-import com.google.android.gms.common.util.ArrayUtils.contains
-import java.util.*
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var mostrarCiudad: String = ""
+    private var mostrarAbiertoCerrado: String = ""
+    private var mostrarEspacio: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +29,10 @@ class MainActivity : AppCompatActivity() {
         setup(email ?: "")
 
 
-        val abiertoCerrado = listOf("-- Seleccione uno --", "Abierto", "Cerrado")
-        val listaCiudades =
-            listOf("-- Seleccione una ciudad --", "Madrid", "Toledo", "Sevilla", "Barcelona")
+        val abiertoCerrado = listOf("--selecciona uno--","Abierto", "Cerrado")
+        val listaCiudades = listOf("--selecciona una ciudad--","Madrid","Barcelona")
+        val listaEspacioCerrado= listOf("--selecciona uno--","Apartamento","Bar","Chalet","Centro de culto","Discoteca","Hotel","Museo","Restaurante")
+        val listaEspacioAbierto= listOf("--selecciona uno--","Azotea","Avenida","Plaza","Polideportivo/piscina","Parque")
 
         val adaptador: ArrayAdapter<String> =
             ArrayAdapter(this, R.layout.spinner_personalizado, listaCiudades)
@@ -38,7 +40,12 @@ class MainActivity : AppCompatActivity() {
         val adaptador2: ArrayAdapter<String> =
             ArrayAdapter(this, R.layout.spinner_personalizado, abiertoCerrado)
         binding.spinnerAbiertoCerrado.adapter = adaptador2
-
+        val adaptador3: ArrayAdapter<String> =
+            ArrayAdapter(this, R.layout.spinner_personalizado, listaEspacioAbierto)
+        binding.spinnerEspacioAbierto.adapter = adaptador3
+        val adaptador4: ArrayAdapter<String> =
+            ArrayAdapter(this, R.layout.spinner_personalizado, listaEspacioCerrado)
+        binding.spinnerEspacioCerrado.adapter = adaptador4
 
         binding.spinnerCiudad.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 
@@ -50,12 +57,20 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
 
-            override fun onItemSelected(p0: AdapterView<*>, p1: View?, clickeado: Int, p3: Long) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Has seleccionado: " + listaCiudades[clickeado],
-                    Toast.LENGTH_LONG
-                ).show()
+            override fun onItemSelected(p0: AdapterView<*>, p1: View?, clickado1: Int, p3: Long) {
+
+                if (clickado1 == 0) {
+
+                }
+                if (clickado1 == 1) {
+                    mostrarCiudad = "Madrid"
+
+                }
+                if (clickado1 == 2) {
+                    mostrarCiudad = "Barcelona"
+
+                }
+
             }
 
         }
@@ -70,47 +85,147 @@ class MainActivity : AppCompatActivity() {
                     ).show()
                 }
 
-                override fun onItemSelected(
-                    p0: AdapterView<*>,
-                    p1: View?,
-                    clickado: Int,
-                    p3: Long
+                override fun onItemSelected(p0: AdapterView<*>, p1: View?, clickado: Int, p3: Long
                 ) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Has seleccionado: " + abiertoCerrado[clickado],
-                        Toast.LENGTH_LONG
-                    ).show()
 
                     if (clickado == 0) {
-                        binding.linearEspacioAbierto.visibility = View.GONE
-                        binding.linearEspacioCerrado.visibility = View.GONE
+                        binding.spinnerEspacioAbierto.visibility = View.GONE
+                        binding.spinnerEspacioCerrado.visibility = View.GONE
                     }
-                    if (clickado == 1) {
-                        binding.linearEspacioAbierto.visibility = View.VISIBLE
-                        binding.linearEspacioCerrado.visibility = View.GONE
+
+                    if (clickado == 1){
+                        mostrarAbiertoCerrado = "Abierto"
+
+                        binding.spinnerEspacioAbierto.visibility = View.VISIBLE
+                        binding.spinnerEspacioCerrado.visibility = View.GONE
+
                     }
-                    if (clickado == 2) {
-                        binding.linearEspacioCerrado.visibility = View.VISIBLE
-                        binding.linearEspacioAbierto.visibility = View.GONE
+                    if (clickado == 2){
+                        mostrarAbiertoCerrado = "Cerrado"
+
+                        binding.spinnerEspacioCerrado.visibility = View.VISIBLE
+                        binding.spinnerEspacioAbierto.visibility = View.GONE
 
                     }
 
                 }
 
             }
-        binding.botonBuscar.setOnClickListener{
-            showMainActivity()
+        binding.spinnerEspacioAbierto.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Seleccione uno para continuar",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>, p1: View?, clickado2: Int, p3: Long) {
+                if (clickado2 == 0) {
+
+                }
+
+                if (clickado2 == 1) {
+                    //mostar = getSpinnersSelections()
+                    mostrarEspacio = "Azotea"
+
+                }
+                if (clickado2 == 2) {
+                    mostrarEspacio = "Avenida"
+
+                }
+                if (clickado2 == 3) {
+                    mostrarEspacio = "Plaza"
+
+                }
+                if (clickado2 == 4) {
+                    mostrarEspacio = "Polideportivo/Piscina"
+
+                }
+                if (clickado2 == 5) {
+                    mostrarEspacio = "Parque"
+
+                }
+            }
+
+        }
+        binding.spinnerEspacioCerrado.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Seleccione uno para continuar",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>, p1: View?, clickado3: Int, p3: Long) {
+
+                if (clickado3 == 0) {
+
+                }
+
+                if (clickado3 == 1) {
+
+                    mostrarEspacio = "Apartamento"
+
+                }
+                if (clickado3 == 2) {
+                    mostrarEspacio = "Bar"
+
+                }
+                if (clickado3 == 3) {
+                    mostrarEspacio = "Chalet"
+
+                }
+                if (clickado3 == 4) {
+                    mostrarEspacio = "Centro de culto"
+
+                }
+                if (clickado3 == 4) {
+                    mostrarEspacio = "Discoteca"
+
+                }
+                if (clickado3 == 5) {
+                    mostrarEspacio = "Hotel"
+
+                }
+                if (clickado3 == 6) {
+                    mostrarEspacio = "Museo"
+
+                }
+                if (clickado3 == 7) {
+                    mostrarEspacio = "Restaurante"
+
+                }
+
+
+
+            }
+
+
+        }
+        binding.botonBuscar.setOnClickListener {
+
+
+            val bundle = Bundle()
+            bundle.putString("Ciudad", mostrarCiudad)
+            bundle.putString("Espacio", mostrarEspacio)
+            bundle.putString("AbiertoCerrado", mostrarAbiertoCerrado)
+
+            val Busquedaintent = Intent(this, BusquedaActivity::class.java)
+            Busquedaintent.putExtras(bundle)
+            startActivity(Busquedaintent)
+
+
+
         }
     }
-    private fun showMainActivity (){
 
-        val mainIntent=Intent(this,MainActivity::class.java).apply {
 
-        }
-        startActivity(mainIntent)
 
-    }
+
     private fun setup(email:String){
 
         binding.tvNombreTextView.text=email
@@ -118,6 +233,12 @@ class MainActivity : AppCompatActivity() {
 
 
 }
+
+
+
+
+
+
 
 
 
